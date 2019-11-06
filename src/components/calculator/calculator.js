85ebec4dfc094bf67f0c20c4fb0aa89fd188c8f4
Jacob Richardson <jacobrrichardson41@gmail.com>
2019-11-06 13:44:41 -0600

    added in the calculator.js and styled the card for the calculator along with the index.css

---
+++ src/components/calculator/calculator.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import './calculator.css'
+/* Main calculator component. Contains our other calculator components. */
+class Calculator extends Component {
+    render () {
+        return (
+            <h1 className="Calculator"></h1>
+        );
+    }
+}
+export default Calculator;  
